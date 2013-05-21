88670320a4195873a0056ade5b99a96c2423e8d0
Steffen Beyer <steffen@beyer.io>
2013-05-21 02:11:04 +0200

    create navbar directive

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,16 @@
 'use strict';
 
 angular.module('properbytesApp') // , ['ui.bootstrap'])
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
     ];
+  })
+  .directive('navbar', function() {
+    return {
+      templateUrl: 'views/navbar.html',
+      // replace: false,
+      // link: function() { console.log('navigation link') },
+    };
   });
