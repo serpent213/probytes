a5bc72b5273436a4e36a12159394cb3c3619f348
Steffen Beyer <steffen@beyer.io>
2013-05-22 00:44:40 +0200

    factor out charting into module, working interval selection

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -4,17 +4,14 @@
 
 angular.module('probytes.controllers', [])
   .controller('MainCtrl', function($scope, trafficData) {
-    $scope.traffic = trafficData.get();
-    $scope.traffic.then(function(data) {
-      $scope.trafficByMonth = data.byMonth;
+    trafficData.get().then(function(data) {
+      $scope.traffic = data;
     });
   })
   .controller('YearlyCtrl', function($scope, $routeParams, trafficData) {
     $scope.year = $routeParams.year;
-    console.log('YearlyCtrl');
   })
   .controller('MonthlyCtrl', function($scope, $routeParams, trafficData) {
     $scope.year = $routeParams.year;
     $scope.month = $routeParams.month;
-    console.log('MonthlyCtrl');
   });
--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -2,92 +2,43 @@
 
 /* Directives */
 
-angular.module('probytes.directives', [])
+angular.module('probytes.directives', ['probytes.charts'])
   .directive('navbar', function() {
     return {
       templateUrl: 'views/navbar.html',
       // replace: false,
     };
   })
-  .directive('monthlyChart', function() {
+  .directive('monthlyChart', function(barChart) {
     return {
       // templateUrl: 'views/navbar.html',
       // replace: false,
       link: function(scope, element, attrs) {
-        scope.$watch('trafficByMonth', function() {
-          if (!scope.trafficByMonth) return '';
+        scope.$watch('traffic', function() {
+          if (!scope.traffic) return '';
 
-          var localDate = new Date(),
-              localYear = localDate.getFullYear(),
-              localMonth = localDate.getMonth() + 1,
-              data = _(_(scope.trafficByMonth[localYear][localMonth]).
+          var data = _(_(scope.traffic.byMonth[scope.year][scope.month]).
                 sortBy(function(d) { return -d.bytes })).
                 map(function(d) { return _(_(d).clone()).extend({ bytes: d.bytes / Math.pow(2, 30) }) });
 
-          var margin = {top: 30, right: 45, bottom: 10, left: 220},
-              rowHeight = 30,
-              width = 940 - margin.left - margin.right, // 940px is Bootstrap container width
-              height = (data.length * rowHeight) - margin.top - margin.bottom;
+          barChart(element, data);
+        });
+      }
+    };
+  })
+  .directive('yearlyChart', function(barChart) {
+    return {
+      // templateUrl: 'views/navbar.html',
+      // replace: false,
+      link: function(scope, element, attrs) {
+        scope.$watch('traffic', function() {
+          if (!scope.traffic) return '';
 
-          var x = d3.scale.linear()
-              .domain([0, d3.max(data, function(d) { return d.bytes })])
-              .range([0, width]);
+          var data = _(_(scope.traffic.byYear[scope.year]).
+                sortBy(function(d) { return -d.bytes })).
+                map(function(d) { return _(_(d).clone()).extend({ bytes: d.bytes / Math.pow(2, 30) }) });
 
-          var y = d3.scale.ordinal()
-              .rangeRoundBands([0, height], .2);
-
-          var xAxis = d3.svg.axis()
-              .scale(x)
-              .orient("top");
-
-          var yAxis = d3.svg.axis()
-              .orient("left");
-
-          var svg = d3.select(element[0]).append("svg")
-              .attr("width", width + margin.left + margin.right)
-              .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-          // x.domain(d3.extent(data, function(d) { return d.bytes; })).nice();
-          y.domain(data.map(function(d) { return d.hostname; }));
-
-          var bars = svg.selectAll(".bar")
-            .data(data);
-
-          bars.enter().append("rect")
-              .attr("class", 'bar')
-              .attr("x", function(d) { return x(0); })
-              .attr("y", function(d) { return y(d.hostname); })
-              .attr("width", function(d) { return Math.abs(x(d.bytes) - x(0)); })
-              .attr("height", y.rangeBand());
-
-          bars.enter().append("text")
-              .attr("class", "hostname")
-              .attr("x", -10)
-              .attr("y", function(d) { return y(d.hostname); })
-              // .attr("dy", y.rangeBand() / 2)
-              .attr("dy", "1.15em")
-              .attr("text-anchor", "end")
-              .text(function(d) { return d.hostname; });
-
-          svg.append("g")
-              .attr("class", "x axis")
-              .call(xAxis);
-
-          svg.append("text")
-              .attr("class", "x axis-label")
-              .attr("text-anchor", "end")
-              .attr("x", width + margin.right - 5)
-              .attr("y", -9)
-              .text("[GiB]");
-
-          svg.append("g")
-              .attr("class", "y axis")
-            .append("line")
-              .attr("x1", x(0))
-              .attr("x2", x(0))
-              .attr("y2", height);
+          barChart(element, data);
         });
       }
     };
