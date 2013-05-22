6da11f7405f5cd3d48014ead08c6d535252cff43
Steffen Beyer <steffen@beyer.io>
2013-05-23 00:54:31 +0200

    interval heading with prev/next links (monthly)

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -14,4 +14,34 @@ angular.module('probytes.controllers', [])
   .controller('MonthlyCtrl', function($scope, $routeParams, trafficData) {
     $scope.year = $routeParams.year;
     $scope.month = $routeParams.month;
+    $scope.$watch('traffic', function() {
+      $scope.prevLink = null;
+      $scope.nextLink = null;
+      if (!$scope.traffic) return;
+
+      var tempDate = new Date($scope.year, $scope.month - 1, 1);
+      tempDate.setMonth(tempDate.getMonth() - 1);
+      var prevMonth = tempDate.getMonth() + 1,
+          prevYear = tempDate.getFullYear();
+      tempDate = new Date($scope.year, $scope.month - 1, 1);
+      tempDate.setMonth(tempDate.getMonth() + 1);
+      var nextMonth = tempDate.getMonth() + 1,
+          nextYear = tempDate.getFullYear();
+
+      if ($scope.traffic.byMonth[prevYear][prevMonth]) {
+        $scope.prevLink = {
+          active: true,
+          month: prevMonth,
+          year: prevYear,
+        };
+      }
+
+      if ($scope.traffic.byMonth[nextYear][nextMonth]) {
+        $scope.nextLink = {
+          active: true,
+          month: nextMonth,
+          year: nextYear,
+        };
+      }
+    });
   });
