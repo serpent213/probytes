63e540a21bc381bd44599d739ac36ce80a65b33a
Steffen Beyer <steffen@beyer.io>
2013-05-30 10:19:36 +0200

    reliable preventDefault for navbar dropdown

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -14,7 +14,7 @@ angular.module('probytes.directives', ['probytes.charts', 'probytes.filters'])
         $timeout(function() { // wait for DOM update
           // avoid navigating to "#"
           $('.navbar .dropdown-toggle').click(function(e) { e.preventDefault(); });
-        }, 0);
+        }, 100); // 0 does not reliably work here
 
         var setActive = function(newLocation) {
           $timeout(function() { // wait for DOM update
