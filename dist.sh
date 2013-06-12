54ac69e2659f04d2af377d5b211bcc951fe58157
Steffen Beyer <steffen@beyer.io>
2013-06-12 07:00:36 +0200

    add packager script

---
+++ dist.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# package proper:bytes distribution
+
+set -e
+
+grunt build
+
+version=`grep version component.json | cut -d '"' -f 4`
+
+cd ..
+tar cjf probytes/probytes-$version.tar.bz2 \
+  --exclude dist/components \
+  --exclude Gemfile.lock \
+  probytes/LICENCE.md \
+  probytes/README.md \
+  probytes/daemon \
+  probytes/dist \
+  probytes/docs
